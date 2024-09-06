7f2cb4e85a6b15e9b8614fbdfe50b36410d84b57
lino <denis.puma@unap.edu.pe>
2024-09-06 16:41:45 -0500

    n

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	dsn := "linox:linox123***@tcp(38.43.133.27:3306)/unap.core"
+	dsn := "root:@tcp(localhost:3306)/unap.core"
 	if dsn == "" {
 		log.Fatal("DATABASE_DSN environment variable is not set")
 	}
